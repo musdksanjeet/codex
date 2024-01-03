5cacd80ddacc0c35b2ee206bd24c120a4a0daa16
sanjeetsingh <musdkinfo@gmail.com>
2024-01-03 18:19:07 +0530

    added

--- application/views/admin/includes/_footer.php
+++ application/views/admin/includes/_footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; <?php echo date('Y') ?> <a href="https://adminlte.io">Recenturesoft Infotech Pvt.Ltd.</a></strong>
+    <strong>Copyright &copy; <?php echo date('Y') ?> <a href="#">Recenturesoft Infotech Pvt.Ltd.</a></strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0
