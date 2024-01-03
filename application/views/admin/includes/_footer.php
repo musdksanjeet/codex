b81a3311fae486f4c23eb3c28d9609ecec51138b
sanjeetsingh <musdkinfo@gmail.com>
2024-01-03 18:22:20 +0530

    added

--- application/views/admin/includes/_footer.php
+++ application/views/admin/includes/_footer.php
@@ -45,7 +45,7 @@
 <!-- AdminLTE App -->
 <script src="<?php echo base_url();?>assets/dist/js/adminlte.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url();?>assets/dist/js/demo.js"></script>
+
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="<?php echo base_url();?>assets/dist/js/pages/dashboard.js"></script>
 <script src="<?php echo base_url();?>assets/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
